129c243beafc4663c9d2abcd521a12cf5f9caff2
rizmii <rizmiseptian@gmail.com>
2025-11-24 11:34:44 +0700

    add komik.js

---
+++ models/komik.js
@@ -0,0 +1,18 @@
+
+
+module.exports= (sequelize, DataTypes)=>{
+    const Komik = sequelize.define("komik",{
+    id: {
+        type:DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    title:{
+        type: DataTypes.STRING,
+    },
+    author:{
+        type:DataTypes.STRING,
+    }
+});
+return Komik;
+}
